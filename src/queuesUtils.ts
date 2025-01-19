9c192518e3e4a0b990d08e018749d158922bbadf
hzumaeta <hzumaeta@gmail.com>
2025-01-19 06:41:39 -0500

    con citas

--- src/queuesUtils.ts
+++ src/queuesUtils.ts
@@ -1,16 +1,17 @@
 import payload from 'payload'
 
 export const updateByHumanQueue = async (doc: any) => {
-  console.log(doc, "sacar human data")
+  console.log(doc, "sacar human dataaaaaaaaaaaaaaaaaaaaa")
   const body: any = {
     humanData: {
       nickName: doc.nickName,
       email: doc.email,
-      humanImageUrl: doc.humanImage.url
+      humanImageUrl: doc.humanImage?.url 
     }
   }
 
   const url = `http://localhost:3001/api/pet-humans/${doc.id}/queue-update`;
+  console.log("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa")
   console.log(body,url)
 
   const response = await fetch(url, {
